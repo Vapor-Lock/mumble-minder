62f48fa7f32e39240b3336129507fb4ec62e55d5
slwst <email@slw.st>
2024-07-20 19:19:25 -0400

    feat: copy users from mumble to a discord channel specified by env, still a work in progress

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/vapor-lock/mumble-minder
 
 go 1.23rc2
+
+require (
+	github.com/bwmarrin/discordgo v0.28.1
+	layeh.com/gumble v0.0.0-20221205141517-d1df60a3cc14
+)
+
+require (
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,68 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"layeh.com/gumble/gumble"
+	"layeh.com/gumble/gumbleutil"
+	"net"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello flake")
+	BotToken := os.Getenv("MUMBLE_MINDER_BOT_TOKEN")
+	TargetChannel := os.Getenv("MUMBLE_MINDER_BOT_CHANNEL")
+
+	keepAlive := make(chan bool)
+	config := gumble.NewConfig()
+	config.Username = os.Getenv("MUMBLE_MINDER_USER")
+	config.Password = os.Getenv("MUMBLE_MINDER_PW")
+	config.Attach(gumbleutil.AutoBitrate)
+	config.Attach(gumbleutil.Listener{
+		Disconnect: func(e *gumble.DisconnectEvent) {
+			keepAlive <- true
+		},
+	})
+
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	client, err := gumble.DialWithDialer(new(net.Dialer), "mumble.vaporlock.space:64738", config, tlsConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	defer client.Disconnect()
+
+	discord, err := discordgo.New("Bot " + BotToken)
+	if err != nil {
+		panic(err)
+	}
+
+	discord.Open()
+	defer discord.Close()
+
+	go func(discord *discordgo.Session) {
+		for _ = range time.Tick(time.Minute) {
+			var userList []string
+			for _, v := range client.Users {
+				userList = append(userList, v.Name)
+			}
+
+			message := strings.Join(userList, "\n")
+			message = fmt.Sprintf("```%s```", message)
+
+			discord.ChannelMessageSend(TargetChannel, message)
+		}
+	}(discord)
+
+	for v := range keepAlive {
+		if v {
+			os.Exit(0)
+		}
+	}
 }
